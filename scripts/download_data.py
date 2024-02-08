f0ab9f05899e9258558758b90017ff66c846b626
Devon Peroutky <devonperoutky@Devons-Mac-mini.local>
2024-02-07 17:40:14 -0800

    Refactor download script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 import PIL
 import praw
@@ -24,6 +25,7 @@ For list of subreddits, grab the top X posts for the year/month/alltime
         --> Create entry for Image & "Caption" aka the comment
 '''
 
+
 @dataclass
 class RedditPostCaption:
     submission_id: str
@@ -34,6 +36,25 @@ class RedditPostCaption:
     image_download_path: str
     image_url: str
 
+    def to_json(self):
+        return {
+            "id": self.submission_id,
+            "image": self.image_download_path,
+            "comment_id": self.comment_id,
+            "conversations": [
+                {
+                    "from": "human",
+                    "value": "<image>\nWrite an insult about this person."
+                },
+                {
+                    "from": "gpt",
+                    "value": self.comment
+                }
+            ]
+        }
+
+    def to_csv(self):
+        return f'{self.submission_id},{self.comment_id},"{self.comment}",{self.image_width},{self.image_height},{self.image_url},{self.image_download_path}'
 
 
 class SubRedditCrawler:
@@ -58,125 +79,26 @@ class SubRedditCrawler:
                 self._print_comment_tree(reply, indent + 1)
 
     def ingest_subreddit_as_csv(self, subreddit_name, num_submissions=None, comments_per_submission=10):
+        out_path = './datasets/raw/'
+        image_captions = self._ingest_subreddit(subreddit_name, out_path, num_submissions, comments_per_submission)
+        print(f'Ingested {len(image_captions)} image-caption pairs from subreddit {subreddit_name}')
+
+        out_file = open(f'{out_path}/full_ds.csv', 'a')
+        for image_caption in image_captions:
+            print(image_caption.to_csv())
+            out_file.write(image_caption.to_csv() + '\n')
+
+        out_file.close()
+
+    def _ingest_subreddit(self, subreddit_name, output_path, num_submissions=None, comments_per_submission=10) -> List[RedditPostCaption]:
 
         # Set up the image directory
         image_dir = './images'
-        ds_dir = './datasets'
-        if not os.path.exists(image_dir):
-            os.makedirs(image_dir)
-
-        # Write the header to the file only if file doesn't exist already
-        if not os.path.exists(f'{ds_dir}/full_ds.csv'):
-            with open(f'{ds_dir}/full_ds.csv', 'w+') as out_file:
-                out_file.write(f'submission_id,comment_id,comment,width,height,image_url,image_path\n')
-
-        for submission in self.reddit_client.subreddit(subreddit_name).top(limit=num_submissions):
-            print('--------------------------------------------------')
-            print(f'Title: {submission.title} (https://reddit.com{submission.permalink})')
-            print(f'Image: {submission.url}')
-            print('--------------------------------------------------')
-            comments = submission.comments
-            print(f'\nComments ({submission.num_comments}): \n\n')
-
-            # if submission.id in the csv file, skip it:
-            if submission.id in open(f'{ds_dir}/full_ds.csv').read():
-                print("Submission already in dataset, skipping")
-                continue
-
-            # Check if submission has media_metadata attribute
-            images = []
-
-            if hasattr(submission, 'media_metadata'):
-                print("Submission has media metadata, downloading the full gallery of images")
-
-                # For each key-value pair in the media_metadata dictionary
-                for key, value in submission.media_metadata.items():
-                    # Get the url of the image
-                    image_url = value.get('s').get('u')
-                    print("Downloading image from " + image_url)
-                    response = requests.get(image_url, stream=True)
-                    with open(f'{image_dir}/{submission.id}_{key}.jpg', 'wb') as out_file:
-                        shutil.copyfileobj(response.raw, out_file)
-
-                    print(f'Downloaded image to {image_dir}/{key}.jpg')
-
-                    # Get height and width of image in pixels
-                    images.append({
-                        'width': value.get('s').get('x'),
-                        'height': value.get('s').get('y'),
-                        'image_path': f'{image_dir}/{submission.id}_{key}.jpg',
-                    })
-                    del response
-            else:
-                print("Downloading image from " + submission.url)
-                response = requests.get(submission.url, stream=True)
-                with open(f'{image_dir}/{submission.id}.jpg', 'wb') as out_file:
-                    shutil.copyfileobj(response.raw, out_file)
-
-                print(f'Downloaded image to {image_dir}/{submission.id}.jpg')
-
-                try:
-                    # Get height and width of image in pixels
-                    im = Image.open(f'{image_dir}/{submission.id}.jpg')
-                    width, height = im.size
-                    images.append({
-                        'width': width,
-                        'height': height,
-                        'image_path': f'{image_dir}/{submission.id}.jpg',
-                    })
-                    del im
-                    del response
-                except PIL.UnidentifiedImageError as e:
-                    print("Error opening image, skipping submission")
-                    continue
-
-            # Grap the specified top level comments
-            top_level_comments = filter(lambda c: not isinstance(c, praw.models.MoreComments), comments[:comments_per_submission])
-
-            # Filter out top-level comments that contain a markdown url in the comment body using regex
-            top_level_comments = list(filter(lambda c: not re.search(r'\[.*\]\(.*\)', c.body), top_level_comments))
-
-            # Filter out top-level comments that contain a markdown url in the comment body using regex
-            top_level_comments = list(filter(lambda c: c.body is not '[deleted]', top_level_comments))
-
-            # For each comment, if the comment body contains "Edit:" or "edit" then remove everything after that
-            for comment in top_level_comments:
-                if 'EDIT:' in comment.body:
-                    comment.body = comment.body.split('EDIT:')[0]
-                if 'Edit:' in comment.body:
-                    comment.body = comment.body.split('Edit:')[0]
-                elif 'edit:' in comment.body:
-                    comment.body = comment.body.split('edit:')[0]
-
-            # Replace newlines with spaces
-            for comment in top_level_comments:
-                comment.body = comment.body.replace('\n', ' ')
-
-            # Escape double quotes in comment body with preprending a double quote
-            for comment in top_level_comments:
-                comment.body = comment.body.replace('"', '""').strip()
-
-            print(f"Going through {len(top_level_comments)} top level comments and {len(images)} images for submission {submission.id}")
-            out_file = open(f'{ds_dir}/full_ds.csv', 'a')
-
-            # Output the top level comments
-            for top_level_comment in sorted(top_level_comments, key=lambda c: c.score, reverse=True):
-                for image in images:
-                    line = f'{submission.id},{top_level_comment.id},"{top_level_comment.body}",{image.get("width")},{image.get("height")},{submission.url},{image.get("image_path")}'
-                    print(line)
-                    out_file.write(line+'\n')
-
-            out_file.close()
-
-    def ingest_subreddit_as_conversation_json(self, subreddit_name, output_path, num_submissions=None, comments_per_submission=10):
-        # Set up the image directory
-        image_dir = './images'
-        ds_dir = './datasets'
         file_path = f'{output_path}/train_reddit_roastme.json'
         if not os.path.exists(image_dir):
             os.makedirs(image_dir)
 
-        examples = []# Write the header to the file only if file doesn't exist already
+        image_captions = []
 
         for submission in self.reddit_client.subreddit(subreddit_name).top(limit=num_submissions):
             print('--------------------------------------------------')
@@ -199,7 +121,8 @@ class SubRedditCrawler:
                 for key, value in submission.media_metadata.items():
                     # Get the url of the image
                     image_url = value.get('s').get('u')
-                    image, width, height, download_path = SubRedditCrawler.download_image(image_url,f'{image_dir}/{submission.id}_key.jpg')
+                    image, width, height, download_path = SubRedditCrawler.download_image(image_url,
+                                                                                          f'{image_dir}/{submission.id}_key.jpg')
                     if image:
                         images.append({
                             'width': width,
@@ -207,7 +130,8 @@ class SubRedditCrawler:
                             'image_path': download_path,
                         })
             else:
-                image, width, height, download_path = SubRedditCrawler.download_image(submission.url, f'{image_dir}/{submission.id}.jpg')
+                image, width, height, download_path = SubRedditCrawler.download_image(submission.url,
+                                                                                      f'{image_dir}/{submission.id}.jpg')
                 if image:
                     images.append({
                         'width': width,
@@ -216,7 +140,8 @@ class SubRedditCrawler:
                     })
 
             # Grap the specified top level comments
-            top_level_comments = filter(lambda c: not isinstance(c, praw.models.MoreComments), comments[:comments_per_submission])
+            top_level_comments = filter(lambda c: not isinstance(c, praw.models.MoreComments),
+                                        comments[:comments_per_submission])
 
             # Filter out top-level comments that contain a markdown url in the comment body using regex
             top_level_comments = list(filter(lambda c: not re.search(r'\[.*\]\(.*\)', c.body), top_level_comments))
@@ -241,38 +166,54 @@ class SubRedditCrawler:
             for comment in top_level_comments:
                 comment.body = comment.body.replace('"', '""').strip()
 
-            print(f"Going through {len(top_level_comments)} top level comments and {len(images)} images for submission {submission.id}")
+            print(
+                f"Going through {len(top_level_comments)} top level comments and {len(images)} images for submission {submission.id}")
 
             # Output the top level comments
             for top_level_comment in sorted(top_level_comments, key=lambda c: c.score, reverse=True):
                 for image in images:
-                    line = f'{submission.id},{top_level_comment.id},"{top_level_comment.body}",{image.get("width")},{image.get("height")},{submission.url},{image.get("image_path")}'
-                    print(line)
+                    image_captions.append(RedditPostCaption(submission.id, top_level_comment.id, top_level_comment.body,
+                                                            image.get("width"), image.get("height"),
+                                                            image.get("image_path"), submission.url))
 
-                    # Structure for LLaVA JSON
-                    json_data = {
-                        "id": submission.id,
-                        "image": image.get("image_path"),
-                        "comment_id": top_level_comment.id,
-                        "conversations": [
-                            {
-                                "from": "human",
-                                "value": "<image>\nWrite an insult about this person."
-                            },
-                            {
-                                "from": "gpt",
-                                "value": top_level_comment.body
-                            }
-                        ]
-                    }
+        return image_captions
 
-                    examples.append(json_data)
+    def ingest_subreddit_as_conversation_json(self, subreddit_name, output_path, num_submissions=None, comments_per_submission=10):
+        image_captions = [reddit_submission.to_json() for reddit_submission in self._ingest_subreddit(subreddit_name, output_path, num_submissions, comments_per_submission)]
+        print(f'Ingested {len(image_captions)} image-caption pairs from subreddit {subreddit_name}')
 
-        # Save the JSON data list to a file
-        json_output_path = os.path.join(output_path, 'train', 'dataset.json')
+        # Split image_captions into train, test, and validation sets
+        train_percentage, test_percentage, validation_percentage = .8, .1, .1
+        assert train_percentage + test_percentage + validation_percentage == 1
+
+        train_size = int(train_percentage * len(image_captions))
+        test_size = int(test_percentage * len(image_captions))
+
+        train_split = image_captions[0:train_size]
+        test_split = image_captions[train_size:train_size + test_size]
+        validation_split = image_captions[train_size+test_size:]
+
+        # Save the full JSON data list to a file
+        json_output_path = os.path.join(output_path, 'full_dataset.json')
         os.makedirs(os.path.dirname(json_output_path), exist_ok=True)
         with open(json_output_path, 'w') as json_file:
-            json.dump(examples, json_file, indent=4)
+            json.dump(image_captions, json_file, indent=4)
+            print(f'Saved {len(image_captions)} image-caption pairs to {json_output_path}')
+
+        json_output_path = os.path.join(output_path, 'train_dataset.json')
+        with open(json_output_path, 'w') as json_file:
+            json.dump(train_split, json_file, indent=4)
+            print(f'Saved {len(train_split)} image-caption pairs to {json_output_path}')
+
+        json_output_path = os.path.join(output_path, 'test_dataset.json')
+        with open(json_output_path, 'w') as json_file:
+            json.dump(test_split, json_file, indent=4)
+            print(f'Saved {len(test_split)} image-caption pairs to {json_output_path}')
+
+        json_output_path = os.path.join(output_path, 'validation_dataset.json')
+        with open(json_output_path, 'w') as json_file:
+            json.dump(validation_split, json_file, indent=4)
+            print(f'Saved {len(validation_split)} image-caption pairs to {json_output_path}')
 
     @staticmethod
     def download_image( image_url, download_path):
